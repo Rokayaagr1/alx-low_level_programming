a309ebb8cd02d83ddf4aeb379f0c1ca3902efda3
Rokayaagr1 <rosayarosy22@gmail.com>
2023-03-18 10:41:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - all the magic is in the main function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
