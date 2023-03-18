e7fe438eec73e289857ae7a8d79e54f5b40474ae
Rokayaagr1 <rosayarosy22@gmail.com>
2023-03-18 10:09:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp = "a";
+while (alp != '{')
+{
+putchar(alp);
+alp++;
+}
+putchar('\n');
+return (0);
+}
